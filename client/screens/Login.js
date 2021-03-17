a9c44abe2314f25c3b0137b565f7ab3d7a5019e8
sudoersfile <tahscenery@gmail.com>
2021-03-17 16:42:14 +1100

    Basic login UI

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function Login() {
   return (
-    <View style={styles}>
-      <Text>Login Screen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.form}>
+          <TextInput style={styles.input} placeholder="email" />
+          <TextInput
+            style={styles.input}
+            placeholder="password"
+            secureTextEntry={true}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  form: {
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "600",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "red",
+    height: 40,
+    marginBottom: 30,
+  },
+});
