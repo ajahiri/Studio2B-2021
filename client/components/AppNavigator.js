5f12e5eb19a6f75fc84bdd9038c36fb6830b222d
sudoersfile <tahscenery@gmail.com>
2021-04-14 15:17:21 +1000

    Resolved AppNavigator error

--- client/components/AppNavigator.js
+++ client/components/AppNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
 import ImageAuthRegistration from '../screens/ImageAuthRegistration';
@@ -15,11 +14,6 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
         <Stack.Screen
           name="Login"
           component={Login}
@@ -39,7 +33,6 @@ const AppNavigator = () => {
           name="ImageAuthRegistration"
           component={ImageAuthRegistration}
           options={{ headerShown: false }}
-
         />
         <Stack.Screen
           name="TeacherDashboard"
