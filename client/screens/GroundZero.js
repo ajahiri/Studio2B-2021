d0cf4fcdcbac441faa5436b071aa03dbaaf76e45
Wai30Yan <42363348+Wai30Yan@users.noreply.github.com>
2021-05-12 21:28:48 +1000

    bottom tab styling

--- client/screens/GroundZero.js
+++ client/screens/GroundZero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Dashboard from './Dashboard';
@@ -9,9 +9,39 @@ const Tab = createBottomTabNavigator();
 
 export default function GroundZero() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          position: 'absolute',
+
+        }
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Text style={{color: focused ? '#e32f45' : '#748c94'}}>DASHBOARD</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Text style={{color: focused ? '#e32f45' : '#748c94'}}>PROFILE</Text>
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
+
+//style={{ flex: 1, justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}
